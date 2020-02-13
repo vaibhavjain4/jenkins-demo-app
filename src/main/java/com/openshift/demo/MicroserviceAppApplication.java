000d9a9d26ec46e45b337af27fa3b688346877c0
Vaibhav Jain <vaibhavjain4@gmail.com>
2020-02-13 18:07:40 +0530

    Version 1.0.0 commit

---
+++ src/main/java/com/openshift/demo/MicroserviceAppApplication.java
@@ -0,0 +1,13 @@
+package com.openshift.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openshift/demo/MicroserviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openshift.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
